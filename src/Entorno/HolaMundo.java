e5fa4b47d6c321cd82df05b24e9589bbdd99900c
Dcuriel <dcurielm01@educarex.es>
2023-11-07 13:47:42 +0100

    Inicio Proyecto

---
+++ src/Entorno/HolaMundo.java
@@ -0,0 +1,12 @@
+package Entorno;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+		System.out.println("Buenos días");
+		System.out.println("Buenas tardes");
+
+	}
+
+}
---
+++ src/Entorno/HolaPersona.java
@@ -0,0 +1,17 @@
+package Entorno;
+
+import java.util.Scanner;
+
+public class HolaPersona {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		String nombre;
+
+		System.out.println("Introduce tu nombre: ");
+		nombre = entrada.next();
+		
+		System.out.println("Buenos días " + nombre);
+	}
+
+}
